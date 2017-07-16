c996d5cc1756ecf626e0156cf25ac9ce06857974
opalh <opalh@users.noreply.github.com>
2017-07-16 13:42:37 +0300

    Add files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -14,22 +14,22 @@ dataSubjectTrain <- tbl_df(read.table(file.path(path, "train", "subject_train.tx
 dataSubjectTest  <- tbl_df(read.table(file.path(path, "test" , "subject_test.txt" )))
 
 # Read activity files
-dataActivityTrain <- tbl_df(read.table(file.path(path, "train", "Y_train.txt")))
-dataActivityTest  <- tbl_df(read.table(file.path(path, "test" , "Y_test.txt" )))
+dataYTrain <- tbl_df(read.table(file.path(path, "train", "Y_train.txt")))
+dataYTest  <- tbl_df(read.table(file.path(path, "test" , "Y_test.txt" )))
 
 #Read data files.
-dataTrain <- tbl_df(read.table(file.path(path, "train", "X_train.txt" )))
-dataTest  <- tbl_df(read.table(file.path(path, "test" , "X_test.txt" )))
+dataXTrain <- tbl_df(read.table(file.path(path, "train", "X_train.txt" )))
+dataXTest  <- tbl_df(read.table(file.path(path, "test" , "X_test.txt" )))
 
 # for both Activity and Subject files this will merge the training and the test sets by row binding 
 #and rename variables "subject" and "activityNum"
 alldataSubject <- rbind(dataSubjectTrain, dataSubjectTest)
 setnames(alldataSubject, "V1", "subject")
-alldataActivity<- rbind(dataActivityTrain, dataActivityTest)
+alldataActivity<- rbind(dataYTrain, dataYTest)
 setnames(alldataActivity, "V1", "activityNum")
 
 #combine the DATA training and test files
-dataTable <- rbind(dataTrain, dataTest)
+dataTable <- rbind(dataXTrain, dataXTest)
 
 # name variables according to feature e.g.(V1 = "tBodyAcc-mean()-X")
 dataFeatures <- tbl_df(read.table(file.path(path, "features.txt")))
@@ -64,15 +64,15 @@ dataTable<- tbl_df(arrange(dataAggr,subject,activityName))
 
 names(dataTable)<-gsub("std()", "SD", names(dataTable))
 names(dataTable)<-gsub("mean()", "MEAN", names(dataTable))
-names(dataTable)<-gsub("^t", "time", names(dataTable))
-names(dataTable)<-gsub("^f", "frequency", names(dataTable))
+names(dataTable)<-gsub("^t", "Time", names(dataTable))
+names(dataTable)<-gsub("^f", "Frequency", names(dataTable))
 names(dataTable)<-gsub("Acc", "Accelerometer", names(dataTable))
 names(dataTable)<-gsub("Gyro", "Gyroscope", names(dataTable))
 names(dataTable)<-gsub("Mag", "Magnitude", names(dataTable))
 names(dataTable)<-gsub("BodyBody", "Body", names(dataTable))
 # Names after
 head(str(dataTable),6)
-head(dataTable,6)
+
 
 
 write.table(dataTable, file = "tidydata.txt",row.name=FALSE)
